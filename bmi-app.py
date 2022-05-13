9e102d1a1e2948edb32784636d2c89c12265ef0f
Dius <57423603+diusdi@users.noreply.github.com>
2022-05-13 16:59:09 +0700

    Update bmi-app.py

--- bmi-app.py
+++ bmi-app.py
@@ -3,41 +3,37 @@ import streamlit as st
 st.write("""
 # Aplikasi Kalkulator BMI
 """)
-tb= float(input("masukkan tinggi badan anda dalam cm="))
-bb= float(input("masukkan berat badan anda dalam kg="))
-umur=float(input("masukkan umur badan anda dalam tahun="))
-JK=float(input("jenis kelamin (1= Laki-laki ; 2=Perempuan) ?"))
 
-print("=============================")
-print("tinggi badan anda", tb)
-print("berat badan anda", bb)
-print("=============================")
+tinggi = st.number_input("Masukkan tinggi badan anda (cm)", 0)
+berat = st.number_input("Masukkan berat badan anda (kg)", 0)
+umur = st.number_input("Masukkan umur anda ", 0)
+JK = st.number_input("Jenis Kelamin [1. laki-laki, 2. perempuan] ", 0)
 
 #Menghitung indeks masa tubuh
-imt=bb/ ((tb/100)*(tb/100))
-print("Indeks Masa Tubuh anda adalah= ",imt)
+imt=berat/ ((tinggi/100)*(tinggi/100))
+st.write("Indeks Masa Tubuh anda adalah= ",imt)
 
 #kategori indeks masa tubuh
-kateg="==="
+kategori=''
 if imt<18.5 :
-    kateg="kurang"
+    kategori="kurang"
 else :
-        if 18.5 <= imt <= 22.9: kateg="Normal"
+        if 18.5 <= imt <= 22.9: kategori="Normal"
         else:
-            if 22.9 < imt <= 29.9: kateg="Berat badan berlebih beresiko obesitas"
-            else: kateg="Obesitas"
-print("kateg =", kateg)
-print("=============================")
+            if 22.9 < imt <= 29.9: kategori="Berat badan berlebih beresiko obesitas"
+            else: kategori="Obesitas"
+st.write("Kategori:", kategori)
+
 
 #Menghitung persentase lemak tubuh
 if JK==1: lemak=(1.2*imt)+(0.23*umur)-10.8-5.4
 if JK==2: lemak=(1.2*imt)+(0.23*umur)-5.4
-print("lemak tubuh anda sebesar ",lemak, " persen")
 
+st.write("Lemak tubuh anda sebesar",lemak, "%")
 
 #Kategori lemak tubuh
 if JK==1:
-    kateg_lemak="==="
+    kateg_lemak=""
     if lemak<=13 :
         kateg_lemak="atlet"
     else :
@@ -47,7 +43,7 @@ if JK==1:
             else: kateg_lemak="Obesitas"
 
 if JK==2:
-    kateg_lemak="==="
+    kateg_lemak=""
     if lemak<=20 :
         kateg_lemak="atlet"
     else :
@@ -55,11 +51,5 @@ if JK==2:
         else:
             if 24 < lemak <= 31: kateg_lemak="Berat badan berlebih beresiko obesitas"
             else: kateg_lemak="Obesitas"
-print("kategori lemak tubuh anda adalah ", kateg_lemak)
 
-
-
-
-
-            
-
+st.write("Kategori lemak tubuh anda adalah ", kateg_lemak)
